9fde51946397607fd6d728824954f71a80dcb00b
Osip Akopiants <osipnunik@gmail.com>
2024-08-15 10:19:19 +0300

    design of grape-item activity xml

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,7 +39,7 @@ android {
 }
 
 dependencies {
-
+    implementation ("com.github.bumptech.glide:glide:4.14.2")
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
--- app/src/main/java/com/example/recycleviewemptyviewactivity/DetailGrapeActivity.kt
+++ app/src/main/java/com/example/recycleviewemptyviewactivity/DetailGrapeActivity.kt
@@ -1,14 +1,32 @@
 package com.example.recycleviewemptyviewactivity
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
+import com.example.recycleviewemptyviewactivity.dao.GrapeDAO
 
 class DetailGrapeActivity : AppCompatActivity(){
 
+    lateinit var grapeDAO : GrapeDAO;
+
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.item_grape_layoutmodel)  // Убедитесь, что target.xml находится в папке res/layout
 
+        val content = findViewById(R.id.content) as TextView
+        content.setText("[Сорт выведен в Англии для оранжерейной культуры]")
+
+        val imageView = findViewById(R.id.image) as ImageView;
+        imageView.setImageResource(R.drawable.mgambur);
+        /*Glide.with(this)
+            .load("https://vinograd.info/images/stories/picss/mgambur.jpg")
+            .into(imageView); */
+
+        //getSupportActionBar().setTitle("Custom Title");
         // Здесь вы можете инициализировать ваши компоненты, например:
         // val textView: TextView = findViewById(R.id.textView)
         // textView.text = "Hello from TargetActivity"
